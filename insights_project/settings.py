9099edb8394a469dd6d5a071c5d0f9f003419e47
mvsanthi123 <mvsanthi.devops@gmail.com>
2026-05-17 08:30:26 +0530

    Update settings.py

--- insights_project/settings.py
+++ insights_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['instareachanalysiss1.onrender.com']
+ALLOWED_HOSTS = ['instareachanalysiss-1.onrender.com']
 
 # Application definition
 
